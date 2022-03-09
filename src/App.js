d0c91ccd68524cb506daec655e93c058ac793810
volter111 <krut.valeriy@gmail.com>
2022-03-09 18:53:23 +0200

    front page layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img className="logoImg" src="/img/logo.png" alt="logo" />
+          <div>
+            <h3 className="text-uppercase">React sneakers</h3>
+            <p className="opacity-5">The best sneakers</p>
+          </div>
+        </div>
+        <div>
+          <ul className="d-flex">
+            <li className="mr-30">
+              <img className="cartSVG" src="/img/cart.svg" alt="logo" />
+              <span>$150</span>
+            </li>
+            <li>
+              <img className="userSVG" src="/img/user.svg" alt="logo" />
+            </li>
+          </ul>
+        </div>
       </header>
+
+      <div className="content p-40">
+        <h1 className="mb-40">All sneakers</h1>
+
+        <div className="sneakers d-flex justify-between">
+
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/example-1.jpg"
+              alt=""
+            />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Price:</span>
+                <b>$125</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/example-2.jpg"
+              alt=""
+            />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Price:</span>
+                <b>$125</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/example-3.jpg"
+              alt=""
+            />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Price:</span>
+                <b>$125</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/example-4.jpg"
+              alt=""
+            />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Price:</span>
+                <b>$125</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
